1c334683da90f0584c19053f2bc5e14601c459ed
juansito606 <juanesizquierdo71@gmail.com>
2025-09-07 03:17:42 +0000

    juanito

--- tarea.java
+++ tarea.java
@@ -20,9 +20,56 @@ public class  Paquete{
 
             boolean ok = oficina.registrarPaquete(f,c,dets);
             if(ok){
-                System.out.println("[OK] Paquete registrado en [" + f + ","]")
+                System.out.println("[OK] Paquete registrado en [" + f + ","+ c +"]");
+            }else {
+                System.out,println("[Error] Casillero ocupado o cordonadas invalidas");
             }
+           System.out.println(oficina.esquemaCasilleros());
+           
+        } else if (opcion == 2) {
+            System.out.println("--Consulta de casilleros disponibles--");
+            System.out.println(oficina.esquemaCasilleros());
+
+        }else if (opcion == 3) {
+            System.out,println("--Informacion de paquetes--");
+            System.out.println(oficina.infoPaquetes());
+
+        }else if (opcion == 4) {
+            System.out.println("Cerrando aplicacion...");
+
+        }
+        
+
+    } while (opcion ! =4);
+
+     sc.close();
+
+}
+
+private static void mostrarMenu() {
+    System.out.println("=============");
+    System.out.println("1. Registrar paquete");
+    System.out.println("2. Consultar casilleros disponibles");
+    System.out.println("3. Informacion de paquetes");
+    System.out.println("4. Salir");
+    System.out.println("=========");
+}
+private static int  leerEntero (Scanner sc, String prompt, int min, int max) {
+    int valor = -1;
+    boolean valido = false;
+    while (!valido){
+        System.out.print(prompt);
+        String linea = sc.nextLine();
+        try{
+            valor = Interger.parseInt(linea);
+            if (valor < min || valor > max) {
+                System.out.println("[Error]Ingrese un valor entre" + min + "y" + max);    
+            }else {
+                valido = true;
+            }
+        }catch (NumberFormatException e) {
+            System.out.println("[Error] Ingrese un numero valido");
         }
     }
-
+    return valor;
 }
